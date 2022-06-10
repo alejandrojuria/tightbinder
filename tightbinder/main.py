85d045d20a88e4ccb5c9df451b6f49a1ad635e07
Alejandro-dell <alejandro.uria@uam.es>
2022-06-10 14:23:16 +0200

    Adapted main to new configfile format

--- tightbinder/main.py
+++ tightbinder/main.py
@@ -8,7 +8,7 @@ import sys
 from tightbinder.fileparse import parse_config_file
 from tightbinder.topology import calculate_wannier_centre_flow, calculate_z2_invariant, plot_wannier_centre_flow
 import time
-from tightbinder.models import SKModel
+from tightbinder.models import SlaterKoster
 import matplotlib.pyplot as plt
 
 
@@ -27,23 +27,19 @@ def main():
         sys.exit(1)
 
     configuration = parse_config_file(file)
-    bi = SKModel(configuration)
-    # lattice.plot_crystal(cell_number=1)
-    labels = ["M", "G", "K", "M"]
-    kpoints = bi.high_symmetry_path(200, labels)
-    bi.initialize_hamiltonian()
+    if 'Radius' in configuration:
+        model = SlaterKoster(configuration, mode='radius', r=configuration['Radius'])
+    else:
+        model = SlaterKoster(configuration)
 
-    results = bi.solve(kpoints)
+    # model.visualize() # Has to be fixed
+    labels = configuration['High symmetry points']
+    kpoints = model.high_symmetry_path(configuration['Mesh'][0], labels)
+    model.initialize_hamiltonian()
+    results = model.solve(kpoints)
     results.plot_along_path(labels)
 
-    bi.filling = 5./8.
-    wcc_flow = calculate_wannier_centre_flow(bi, number_of_points=10)
-    plot_wannier_centre_flow(wcc_flow, show_midpoints=True)
-    print(f"Z2 invariant: {calculate_z2_invariant(wcc_flow)}")
-
-    plot_wannier_centre_flow(wcc_flow)
     plt.show()
-
 
 if __name__ == "__main__":
     initial_time = time.time()
