4f57ef1c9bfcc021ebe5ec47fc0393a16aeb6276
Alejandro <alejandro.uria@uam.es>
2022-10-10 19:43:14 +0200

    Change autohints package

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,8 +28,7 @@ author = 'Alejandro Jose Uria Alvarez'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-              'sphinx_autodoc_typehints',
+extensions = ['sphinx_toolbox.more_autodoc.typehints',
               'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
