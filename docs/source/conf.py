6c2f1c00c0a030e27bb62ee346917025c6c2b6b1
Alejandro <alejandro.uria@uam.es>
2022-10-10 21:40:05 +0200

    Added yaml file for readthedocs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,8 @@ author = 'Alejandro Jose Uria Alvarez'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_toolbox.more_autodoc.typehints',
+extensions = ['sphinx.ext.autodoc',
+              'sphinx_autodoc_typehints',
               'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
